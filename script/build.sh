1c2c81ab339aa7e79b1505b7bb855912495539f0
ysd-ktn <yosida.kotone@gmail.com>
2021-06-05 19:31:41 +0900

    build.shの修正

--- script/build.sh
+++ script/build.sh
@@ -8,7 +8,15 @@ build="${base}/build"
 # 静的化
 python "${base}/freeze.py"
 
+if [ ! -e $base/internship ];then
+  mkdir $base/internship
+fi
+if [ ! -e $base/uichallenge ];then
+  mkdir $base/uichallenge
+fi
+
 mv $build/*.html $base
-mv $build/designpages/*.html $base/designpages
+mv $build/internship/*.html $base/internship
+mv $build/uichallenge/*.html $base/uichallenge 
 mv $build/developpages/*.html $base/developpages
 rm -rf $build
